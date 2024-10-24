6ddfb23ce11d09a4058d522561722019451bc745
Alexis Diaz <112739180+ShandeAlexis@users.noreply.github.com>
2024-10-23 23:48:14 -0500

    Update Documento.php

--- app/Models/Documento.php
+++ app/Models/Documento.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Documento extends Model
 {
     //
-    public function documento(){
-        return $this->belongsTo(Documento::class);
+    public function persona(){
+        return $this->hasOne(Persona::class);
     }
 }
