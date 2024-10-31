1ae920d028fef90743d2767baa248aaf6358afab
Alexis Diaz <alexisshande1@gmail.com>
2024-10-30 22:02:27 -0500

    Añadido crud Marca

--- app/Http/Controllers/marcaController.php
+++ app/Http/Controllers/marcaController.php
@@ -2,7 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreMarcaRequest;
+use App\Http\Requests\UpdateMarcaRequest;
+use App\Models\Caracteristica;
+use App\Models\Marca;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class marcaController extends Controller
 {
@@ -11,7 +17,8 @@ class marcaController extends Controller
      */
     public function index()
     {
-        //
+        $marcas = Marca::with('caracteristica')->latest()->get();
+        return view('marcas.index', ['marcas' => $marcas]);
     }
 
     /**
@@ -19,15 +26,25 @@ class marcaController extends Controller
      */
     public function create()
     {
-        //
+        return view('marcas.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreMarcaRequest $request)
     {
-        //
+        try {
+            DB::beginTransaction();
+            $caracteristica = Caracteristica::create($request->validated());
+            $caracteristica->marca()->create([
+                'caracteristica_id' => $caracteristica->id
+            ]);
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+        }
+        return redirect()->route('marcas.index')->with('success', 'Marca registrada');
     }
 
     /**
@@ -41,17 +58,19 @@ class marcaController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Marca $marca)
     {
-        //
+        return view('marcas.edit', ['marca' => $marca]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateMarcaRequest $request, Marca $marca)
     {
-        //
+        Caracteristica::where('id', $marca->caracteristica->id)
+        ->update($request->validated());
+    return redirect()->route('marcas.index')->with('success', 'Marca Editada');
     }
 
     /**
@@ -59,6 +78,23 @@ class marcaController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $message = '';
+        $marca = Marca::find($id);
+
+        if ($marca->caracteristica->estado == 1) {
+            Caracteristica::where('id', $marca->caracteristica->id)
+                ->update([
+                    'estado' => 0
+                ]);
+                $message= 'Marca Eliminada';
+        } else {
+            Caracteristica::where('id', $marca->caracteristica->id)
+                ->update([
+                    'estado' => 1
+                ]);
+                $message= 'Marca restaurada';
+        }
+
+        return redirect()->route('marcas.index')->with('success', $message);
     }
 }
